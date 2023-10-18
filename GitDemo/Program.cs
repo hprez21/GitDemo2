fb65660f4c19f92c56686469d53ba368e744c28a
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2023-10-18 16:51:08 -0600

    Commiting to branch01

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Git!!!");
+app.MapGet("/", () => "Hello Git!!! - branch01");
 
 app.Run();
