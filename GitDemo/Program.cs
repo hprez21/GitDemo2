e1f7add853d8333112d1482c0c449198a2f907e7
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2023-10-19 17:42:46 -0600

    Text fixed #1

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Git!!! - branch01");
+app.MapGet("/", () => "Hello Git!");
 
 app.Run();
